c21d0bc01a920d6fca60ec556a8931b12f601b5b
jicking <jdbebiro@gmail.com>
2015-08-03 13:07:01 +0800

    update connection Signed-off-by: jicking <jdbebiro@gmail.com>

--- HapiWifi.Web/Models/IdentityModels.cs
+++ HapiWifi.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace HapiWifi.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("aspidentitydb", throwIfV1Schema: false)
         {
         }
 
