c0a8578d1b087071947a555fa439e31189a0c94d
jicking <jdbebiro@gmail.com>
2015-08-24 14:01:50 +0800

    added checking for mobile. Will add logs view after this. Signed-off-by: jicking <jdbebiro@gmail.com>

--- HapiWifi.Web/app_code/RequestLogger.cs
+++ HapiWifi.Web/app_code/RequestLogger.cs
@@ -24,6 +24,7 @@ namespace HapiWifi.Web.app_code
             int majorVersion = 0;
             string userAgent = string.Empty;
             string page = string.Empty;
+            bool isMobile = false;
 
             if (HttpContext.Current.Request.Browser != null)
             {
@@ -46,11 +47,14 @@ namespace HapiWifi.Web.app_code
                 page = HttpContext.Current.Request.Url.AbsoluteUri;
             }
 
+            isMobile = this.CheckIfMobileRequestHeader(userAgent);
+
             //SAVE REQUIRED DATA
             HapiWifiDB db = new HapiWifiDB();
             RequestLog log = new RequestLog();
             log.Date = DateTime.Now;
             log.Page = page;
+            log.IsMobileRequest = isMobile;
             log.Browser = string.Format("{0} {1}|| {2}",browser,majorVersion,userAgent);
             db.RequestLogs.Add(log);
             db.SaveChanges();
@@ -65,5 +69,19 @@ namespace HapiWifi.Web.app_code
             base.OnActionExecuting(filterContext);
         }
 
+        private bool CheckIfMobileRequestHeader(string useragent)
+        {
+            string[] mobileheaders = { "android", "webos", "iphone", "ipad", "ipod", "blackberry", "windows phone" };
+
+            foreach (var item in mobileheaders)
+            {
+                if (useragent.Contains(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
